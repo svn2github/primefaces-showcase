ced50bcdb152649d49e40445d7b8a5733f07bdb2
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-07-29 20:48:51 +0000

    AutoComplete example update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@3177 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/PlayerConverter.java
+++ src/main/java/org/primefaces/examples/view/PlayerConverter.java
@@ -17,47 +17,56 @@ package org.primefaces.examples.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 
 import org.primefaces.examples.domain.Player;
 
 public class PlayerConverter implements Converter {
 
-	private static List<Player> playerDB;
-	
-	static {
-		playerDB = new ArrayList<Player>();
-		
-		playerDB.add(new Player("Messi", 10));
-		playerDB.add(new Player("Ibrahimovic", 9));
-		playerDB.add(new Player("Henry", 14));
-		playerDB.add(new Player("Iniesta", 8));
-		playerDB.add(new Player("Xavi", 6));
-		playerDB.add(new Player("Puyol", 5));
-	}
-	
-	public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {
-		if(submittedValue.trim().equals(""))
-			return null;
-		else {
-			int number = Integer.parseInt(submittedValue);
-			
-			for(Player p : playerDB) {
-				if(p.getNumber() == number)
-					return p;
-			}
-		}
-		
-		return null;
-	}
+    private static List<Player> playerDB;
 
-	public String getAsString(FacesContext facesContext, UIComponent component, Object value) {
-		if(value == null)
-			return null;
-		else
-			return String.valueOf(((Player) value).getNumber());
-	}
+    static {
+        playerDB = new ArrayList<Player>();
+
+        playerDB.add(new Player("Messi", 10));
+        playerDB.add(new Player("Ibrahimovic", 9));
+        playerDB.add(new Player("Henry", 14));
+        playerDB.add(new Player("Iniesta", 8));
+        playerDB.add(new Player("Xavi", 6));
+        playerDB.add(new Player("Puyol", 5));
+    }
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String submittedValue) {
+        if (submittedValue.trim().equals("")) {
+            return null;
+        } else {
+            try {
+                int number = Integer.parseInt(submittedValue);
+
+                for (Player p : playerDB) {
+                    if (p.getNumber() == number) {
+                        return p;
+                    }
+                }
+                
+            } catch(NumberFormatException exception) {
+                throw new ConverterException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Conversion Error", "Not a valid player"));
+            }
+        }
+
+        return null;
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object value) {
+        if (value == null) {
+            return null;
+        } else {
+            return String.valueOf(((Player) value).getNumber());
+        }
+    }
 }
