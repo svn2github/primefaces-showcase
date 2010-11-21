59e296138d05b428e511683c1ff1d4f2b65e3482
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-11-21 13:58:46 +0000

    Rating sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@3749 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/RatingController.java
+++ src/main/java/org/primefaces/examples/view/RatingController.java
@@ -7,45 +7,39 @@ import org.primefaces.event.RateEvent;
 
 public class RatingController {
 
-	private Double rating1;
+	private double rating1;
 
-	private Double rating2;
+	private double rating2;
 	
-	private Double rating3 = Double.valueOf(3);
+	private double rating3 = 3;
 	
 	public void handleRate(RateEvent rateEvent) {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Rate Event", null);
-		Double rating = rateEvent.getRating();
-		
-		if(rating != null)
-			message.setDetail("You rated:" + rateEvent.getRating());
-		else
-			message.setDetail("You cleared your rate");
-		
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Rate Event", "You rated:" + (int) rateEvent.getRating());
+
 		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 	
-	public Double getRating1() {
+	public double getRating1() {
 		return rating1;
 	}
 
-	public void setRating1(Double rating1) {
+	public void setRating1(double rating1) {
 		this.rating1 = rating1;
 	}
 
-	public Double getRating2() {
+	public double getRating2() {
 		return rating2;
 	}
 
-	public void setRating2(Double rating2) {
+	public void setRating2(double rating2) {
 		this.rating2 = rating2;
 	}
 
-	public Double getRating3() {
+	public double getRating3() {
 		return rating3;
 	}
 
-	public void setRating3(Double rating3) {
+	public void setRating3(double rating3) {
 		this.rating3 = rating3;
 	}
 }
