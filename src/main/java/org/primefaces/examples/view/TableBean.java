2748897ad3b5fdf0b6b3267442a16791ab6b4c7f
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-01-21 13:16:30 +0000

    Updated exporter sample
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@3859 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/TableBean.java
+++ src/main/java/org/primefaces/examples/view/TableBean.java
@@ -44,6 +44,8 @@ import com.lowagie.text.BadElementException;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Rectangle;
 import java.util.Map;
 import javax.faces.model.SelectItem;
 import org.primefaces.event.DragDropEvent;
@@ -264,6 +266,9 @@ public class TableBean implements Serializable {
 	
 	public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
 		Document pdf = (Document) document;
+        pdf.open();
+        pdf.setPageSize(PageSize.A4);
+
 		ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
 		String logo = servletContext.getRealPath("") + File.separator + "images" + File.separator + "prime_logo.png";
 		
