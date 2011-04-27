75318ec2a1c9de603e95bdbe87e227c464239763
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-04-27 14:41:36 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@4507 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ColorBean.java
+++ src/main/java/org/primefaces/examples/view/ColorBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Prime Technology.
+ * Copyright 2009-2011 Prime Technology.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,47 +15,45 @@
  */
 package org.primefaces.examples.view;
 
-import org.primefaces.model.Color;
-
 public class ColorBean {
 
-	private Color color1;
+	private String color1;
 
-    private Color color2;
+    private String color2;
 
-    private Color color3;
+    private String color3;
 
-    private Color color4;
+    private String color4;
 
-    public Color getColor1() {
+    public String getColor1() {
         return color1;
     }
 
-    public void setColor1(Color color1) {
+    public void setColor1(String color1) {
         this.color1 = color1;
     }
 
-    public Color getColor2() {
+    public String getColor2() {
         return color2;
     }
 
-    public void setColor2(Color color2) {
+    public void setColor2(String color2) {
         this.color2 = color2;
     }
 
-    public Color getColor3() {
+    public String getColor3() {
         return color3;
     }
 
-    public void setColor3(Color color3) {
+    public void setColor3(String color3) {
         this.color3 = color3;
     }
 
-    public Color getColor4() {
+    public String getColor4() {
         return color4;
     }
 
-    public void setColor4(Color color4) {
+    public void setColor4(String color4) {
         this.color4 = color4;
     }
 }
