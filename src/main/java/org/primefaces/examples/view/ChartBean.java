2dd1ba20beab53e490bea43ced695e97b83069dd
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-05-10 09:02:00 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@4544 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ChartBean.java
+++ src/main/java/org/primefaces/examples/view/ChartBean.java
@@ -31,14 +31,6 @@ import org.primefaces.model.chart.PieChartModel;
 
 public class ChartBean implements Serializable {
 
-	private List<Sale> sales;
-	
-	private List<Birth> births;
-	
-	private List<Vote> votes;
-
-    private List<ServerLoad> servers;
-
     private CartesianChartModel cartesianModel;
 
     private PieChartModel pieModel;
@@ -46,58 +38,10 @@ public class ChartBean implements Serializable {
     private PieChartModel livePieModel;
 
 	public ChartBean() {
-		sales = new ArrayList<Sale>();
-		sales.add(new Sale("Brand 1", 540));
-		sales.add(new Sale("Brand 2", 325));
-		sales.add(new Sale("Brand 3", 702));
-		sales.add(new Sale("Brand 4", 421));
-		
-		births = new ArrayList<Birth>();
-		births.add(new Birth(2004, 120, 52));
-		births.add(new Birth(2005, 100, 60));
-		births.add(new Birth(2006, 44, 110));
-		births.add(new Birth(2007, 150, 135));
-		births.add(new Birth(2008, 125, 120));
-		
-		votes = new ArrayList<Vote>();
-		votes.add(new Vote("Candidate 1", 100));
-		votes.add(new Vote("Candidate 2", 100));
-
-        servers = new ArrayList<ServerLoad>(4);
-        for (int i = 0; i < 4; i++) {
-            servers.add(i, new ServerLoad("Server " + i, (int)(Math.random() * 100), (int)(Math.random() * 100)));
-        }
-
         createCartesianModel();
         createPieModel();
         createLivePieModel();
 	}
-
-	public List<Sale> getSales() {
-		return sales;
-	}
-	
-	public List<Birth> getBirths() {
-		return births;
-	}
-	
-	public List<Vote> getVotes() {
-		int random1 = (int)(Math.random() * 1000);
-		int random2 = (int)(Math.random() * 1000);
-	
-		votes.get(0).add(random1);
-		votes.get(1).add(random2);
-		
-		return votes;
-	}
-
-    public List<ServerLoad> getServers() {
-        for (int i = 0; i < 4; i++) {
-            servers.get(i).update( (int)(Math.random() * 100), (int)(Math.random() * 100));
-        }
-
-        return servers;
-    }
 
 	public void itemSelect(ItemSelectEvent event) {
         FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
