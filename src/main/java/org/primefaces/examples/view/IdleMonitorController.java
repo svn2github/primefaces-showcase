eb36d40a139df3c43f76da8765475dbef35cd04d
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-05-26 08:11:13 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@4620 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/IdleMonitorController.java
+++ src/main/java/org/primefaces/examples/view/IdleMonitorController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Prime Technology.
+ * Copyright 2009-2011 Prime Technology.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,14 +18,17 @@ package org.primefaces.examples.view;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-import org.primefaces.event.IdleEvent;
-
 public class IdleMonitorController {
 	
-	public void idleListener(IdleEvent event) {
+	public void idleListener() {
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, 
 										"Your session is closed", "You have been idle for at least 5 seconds"));
 		
 		//invalidate session
+	}
+
+    public void activeListener() {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+										"Welcome Back", "That's a long coffee break!"));
 	}
 }
