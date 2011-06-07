f64c2692d3bab344c3f3d53e99949a7fb2cb451e
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-06-07 09:46:04 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@4693 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/DynamicImageController.java
+++ src/main/java/org/primefaces/examples/view/DynamicImageController.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.primefaces.examples.view;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.BarcodeImageHandler;
@@ -18,58 +36,47 @@ import org.primefaces.model.StreamedContent;
 
 public class DynamicImageController {
 
-	private StreamedContent image;
+	private StreamedContent graphicText;
 	
 	private StreamedContent barcode;
 	
 	private StreamedContent chart;
 
-	private static final Logger logger = Logger.getLogger(DynamicImageController.class.getName());
-
 	public DynamicImageController() {
-		//Database image simulation
-		InputStream stream = this.getClass().getResourceAsStream("optimusprime.jpg");
-		image = new DefaultStreamedContent(stream, "image/jpeg");
-		
-		//Chart
-		try {
-			JFreeChart jfreechart = ChartFactory.createPieChart("Turkish Cities", createDataset(), true, true, false);
-			File chartFile = new File("dynamichart");
-			ChartUtilities.saveChartAsPNG(chartFile, jfreechart, 375, 300);
-			chart = new DefaultStreamedContent(new FileInputStream(chartFile), "image/png");
-		} catch (Exception e) {
-			logger.severe(e.getMessage());
-		}
-	
-		//Barcode
-		try {
+        try {
+            //Graphic Text
+            BufferedImage bufferedImg = new BufferedImage(100, 25, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2 = bufferedImg.createGraphics();
+            g2.drawString("This is a text", 0, 10);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImg, "png", os);
+            graphicText = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "image/png"); 
+
+            //Chart
+            JFreeChart jfreechart = ChartFactory.createPieChart("Turkish Cities", createDataset(), true, true, false);
+            File chartFile = new File("dynamichart");
+            ChartUtilities.saveChartAsPNG(chartFile, jfreechart, 375, 300);
+            chart = new DefaultStreamedContent(new FileInputStream(chartFile), "image/png");
+
+            //Barcode
 			File barcodeFile = new File("dynamicbarcode");
 			BarcodeImageHandler.saveJPEG(BarcodeFactory.createCode128("PRIMEFACES"), barcodeFile);
 			barcode = new DefaultStreamedContent(new FileInputStream(barcodeFile), "image/jpeg");
 		} catch (Exception e) {
-			logger.severe(e.getMessage());
+			e.printStackTrace();
 		}
 	}
 	
 	public StreamedContent getBarcode() {
 		return barcode;
 	}
-	public void setBarcode(StreamedContent barcode) {
-		this.barcode = barcode;
-	}
-	
-	public StreamedContent getImage() {
-		return image;
-	}
-	public void setImage(StreamedContent image) {
-		this.image = image;
-	}
-	
+
+    public StreamedContent getGraphicText() {
+        return graphicText;
+    }
+		
 	public StreamedContent getChart() {
 		return chart;
-	}
-	public void setChart(StreamedContent chart) {
-		this.chart = chart;
 	}
 	
 	private PieDataset createDataset() {
