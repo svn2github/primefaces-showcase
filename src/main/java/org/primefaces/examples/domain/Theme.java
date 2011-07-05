3592bdade2b8625ad94d09567676516f6dc6b883
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-07-05 21:07:48 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@4849 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ src/main/java/org/primefaces/examples/domain/Theme.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2009 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.examples.domain;
+
+import java.io.Serializable;
+
+public class Theme implements Serializable {
+    
+    private String name;
+    
+    private String image;
+    
+    public Theme() {}
+
+    public Theme(String name, String image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/org/primefaces/examples/view/ThemeSwitcherBean.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.examples.view;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.annotation.PostConstruct;
+import org.primefaces.examples.domain.Theme;
+
+public class ThemeSwitcherBean {
+        
+    private Map<String, String> themes;
+    
+    private List<Theme> advancedThemes;
+    
+    private String theme;
+    
+    private GuestPreferences gp;
+
+    public void setGp(GuestPreferences gp) {
+        this.gp = gp;
+    }
+    
+    public Map<String, String> getThemes() {
+        return themes;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    @PostConstruct
+    public void init() {
+        theme = gp.getTheme();
+        
+        advancedThemes = new ArrayList<Theme>();
+        advancedThemes.add(new Theme("aristo", "aristo.png"));
+        advancedThemes.add(new Theme("cupertino", "cupertino.png"));
+        advancedThemes.add(new Theme("trontastic", "trontastic.png"));
+        
+        themes = new TreeMap<String, String>();
+        themes.put("Aristo", "aristo");
+        themes.put("Black-Tie", "black-tie");
+        themes.put("Blitzer", "blitzer");
+        themes.put("Bluesky", "bluesky");
+        themes.put("Casablanca", "casablanca");
+        themes.put("Cupertino", "cupertino");
+        themes.put("Dark-Hive", "dark-hive");
+        themes.put("Dot-Luv", "dot-luv");
+        themes.put("Eggplant", "eggplant");
+        themes.put("Excite-Bike", "excite-bike");
+        themes.put("Flick", "flick");
+        themes.put("Glass-X", "glass-x");
+        themes.put("Hot-Sneaks", "hot-sneaks");
+        themes.put("Humanity", "humanity");
+        themes.put("Le-Frog", "le-frog");
+        themes.put("Midnight", "midnight");
+        themes.put("Mint-Choc", "mint-choc");
+        themes.put("Overcast", "overcast");
+        themes.put("Pepper-Grinder", "pepper-grinder");
+        themes.put("Redmond", "redmond");
+        themes.put("Rocket", "rocket");
+        themes.put("Sam", "sam");
+        themes.put("Smoothness", "smoothness");
+        themes.put("South-Street", "south-street");
+        themes.put("Start", "start");
+        themes.put("Sunny", "sunny");
+        themes.put("Swanky-Purse", "swanky-purse");
+        themes.put("Trontastic", "trontastic");
+        themes.put("UI-Darkness", "ui-darkness");
+        themes.put("UI-Lightness", "ui-lightness");
+        themes.put("Vader", "vader");
+    }
+    
+    public void saveTheme() {
+        gp.setTheme(theme);
+    }
+
+    public List<Theme> getAdvancedThemes() {
+        return advancedThemes;
+    }
+}
