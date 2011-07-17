076a4923619eafa6afcdc75dfea2d549cb86d6c2
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-07-17 14:38:59 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@4951 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ src/main/java/org/primefaces/examples/view/TimelineBean.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.examples.view;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import org.primefaces.model.timeline.Timeline;
+
+public class TimelineBean implements Serializable {
+ 
+    private List<Timeline> model;
+    
+    public TimelineBean() {
+        model = new ArrayList<Timeline>();
+    }
+
+    public List<Timeline> getModel() {
+        return model;
+    }
+}
