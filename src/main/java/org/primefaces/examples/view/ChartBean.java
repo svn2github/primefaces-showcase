7916da9babedade033fc00673949b40b63a3d6db
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-08-11 13:04:08 +0000

    Sample Update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@5238 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ChartBean.java
+++ src/main/java/org/primefaces/examples/view/ChartBean.java
@@ -214,13 +214,13 @@ public class ChartBean implements Serializable {
     private void createBubbleModel(){
         bubbleModel = new BubbleChartModel();
         
-        BubbleChartSeries bubble1 = new BubbleChartSeries("Acura", 70, 183, 1236);
-        BubbleChartSeries bubble2 = new BubbleChartSeries("Alfa Romeo", 45, 92, 1067);
-        BubbleChartSeries bubble3 = new BubbleChartSeries("AM General", 24, 104, 1176);
-        BubbleChartSeries bubble4 = new BubbleChartSeries("Bugatti", 50, 123, 610);
-        BubbleChartSeries bubble5 = new BubbleChartSeries("BMW", 15, 89, 864);
-        BubbleChartSeries bubble6 = new BubbleChartSeries("Audi", 40, 180, 539);
-        BubbleChartSeries bubble7 = new BubbleChartSeries("Aston Martin", 70, 70, 1026);
+        BubbleChartSeries bubble1 = new BubbleChartSeries("Acura", 70, 183,55);
+        BubbleChartSeries bubble2 = new BubbleChartSeries("Alfa Romeo", 45, 92, 36);
+        BubbleChartSeries bubble3 = new BubbleChartSeries("AM General", 24, 104, 40);
+        BubbleChartSeries bubble4 = new BubbleChartSeries("Bugatti", 50, 123, 60);
+        BubbleChartSeries bubble5 = new BubbleChartSeries("BMW", 15, 89, 25);
+        BubbleChartSeries bubble6 = new BubbleChartSeries("Audi", 40, 180, 80);
+        BubbleChartSeries bubble7 = new BubbleChartSeries("Aston Martin", 70, 70, 48);
         
         bubbleModel.addBubble(bubble1);
         bubbleModel.addBubble(bubble2);
