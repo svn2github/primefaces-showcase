d398323b60ffc3c08ec81a3c68b0f50ca402b219
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-01-11 21:54:08 +0000

    Integration Tests - Dock
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@6426 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ src/test/java/com/prime/showcase/integration/dock/DockIntegrationTest.java
@@ -0,0 +1,77 @@
+package com.prime.showcase.integration.dock;
+
+import static org.junit.Assert.assertTrue;
+import com.prime.showcase.integration.AbstractIntegrationTest;
+import com.prime.showcase.integration.SeleniumActionHelper;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+public class DockIntegrationTest extends AbstractIntegrationTest {
+    
+    protected static SeleniumActionHelper action;
+    
+    protected static List<WebElement> items;
+    
+    @Test
+    public void shouldInit(){
+        driver.get(toShowcaseUrl("dock.jsf"));
+        action = new SeleniumActionHelper(driver);
+        items = new ArrayList<WebElement>();
+    }
+    
+    @Test
+    public void shouldVerifyDOM(){
+        
+        try{
+            
+            //look top dock
+            items.add(findElementBySelector("div#dockTop a#top0"));
+            items.add(findElementBySelector("div#dockTop a#top1"));
+            items.add(findElementBySelector("div#dockTop a#top2"));
+            items.add(findElementBySelector("div#dockTop a#top3"));
+            items.add(findElementBySelector("div#dockTop a#top4"));
+            items.add(findElementBySelector("div#dockTop a#top5"));
+            items.add(findElementBySelector("div#dockTop a#top6"));
+            items.add(findElementBySelector("div#dockTop a#top7"));
+
+            //look bottom dock
+            items.add(findElementBySelector("div#dockBottom a#bottom0"));
+            items.add(findElementBySelector("div#dockBottom a#bottom1"));
+            items.add(findElementBySelector("div#dockBottom a#bottom2"));
+            items.add(findElementBySelector("div#dockBottom a#bottom3"));
+            items.add(findElementBySelector("div#dockBottom a#bottom4"));
+            items.add(findElementBySelector("div#dockBottom a#bottom5"));
+            items.add(findElementBySelector("div#dockBottom a#bottom6"));
+            items.add(findElementBySelector("div#dockBottom a#bottom7"));
+           
+        }
+        catch(NoSuchElementException e){
+            assertTrue(false);
+        }
+    }
+    
+    @Test
+    public void shouldVisitItems() throws InterruptedException {
+        
+        for (WebElement e : items) {
+            
+            
+            double baseWidth = Double.parseDouble(e.getCssValue("width").replaceAll("px", "")),
+            baseLeft = Double.parseDouble(e.getCssValue("left").replaceAll("px", ""));
+            
+            action.mouseHover(e);
+            
+            double hoverWidth = Double.parseDouble(e.getCssValue("width").replaceAll("px", "")),
+            hoverLeft = Double.parseDouble(e.getCssValue("left").replaceAll("px", ""));
+            
+            if(hoverWidth <= baseWidth || hoverLeft > baseLeft){
+                assertTrue(false);
+            }
+        }
+    }
+    
+}
