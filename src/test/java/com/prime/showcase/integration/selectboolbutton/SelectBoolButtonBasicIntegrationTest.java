b4c9148135a73863848ace74a2a997283ad99914
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-01-18 19:45:49 +0000

    Integration Tests - SelectBoolButton hidden inputs control.
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@6556 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/test/java/com/prime/showcase/integration/selectboolbutton/SelectBoolButtonBasicIntegrationTest.java
+++ src/test/java/com/prime/showcase/integration/selectboolbutton/SelectBoolButtonBasicIntegrationTest.java
@@ -40,16 +40,22 @@ public class SelectBoolButtonBasicIntegrationTest extends AbstractIntegrationTes
     public void shouldUseButtons() {
 
         WebElement v1 = findElementById("form:value1");
+        WebElement v1_input = findElementBySelector(v1, "input");
+        
         WebElement v2 = findElementById("form:value2");
+        WebElement v2_input = findElementBySelector(v2, "input");
 
         assertThat("Value1 should be no.", v1.getText(), equalTo("No"));
-
+        assertTrue("Value1 should have valid hidden input element.", !v1_input.isSelected());
+        
         v1.click();
 
         assertThat("Value1 should be yes.", v1.getText(), equalTo("Yes"));
-
+        
+        assertTrue("Value1 should toggle hidden input on change.", v1_input.isSelected());
 
         assertThat("Value2 should be no.", v2.getText(), equalTo("No"));
+        assertTrue("Value2 should have valid hidden input element.", !v2_input.isSelected());
 
         try {
             assertTrue("Value2 should display icon.", findElementBySelector(v2, ".ui-icon.ui-icon-close").isDisplayed());
@@ -60,6 +66,8 @@ public class SelectBoolButtonBasicIntegrationTest extends AbstractIntegrationTes
         v2.click();
         
         waitUntilAjaxRequestCompletes();
+        
+        assertTrue("Value2 should toggle hidden input on change.", v2_input.isSelected());
         
         assertThat("Value2 should be yes.", v2.getText(), equalTo("Yes"));
         
