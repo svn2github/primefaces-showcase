0a5433733d84fbdcc36773a2e6c1209ce4eaa696
omergok <omergok@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-01-23 01:45:48 +0000

    Separator integration test
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@6621 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ src/test/java/com/prime/showcase/integration/separator/SeparatorIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.prime.showcase.integration.separator;
+
+import static junit.framework.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.prime.showcase.integration.AbstractIntegrationTest;
+
+public class SeparatorIntegrationTest extends AbstractIntegrationTest {
+
+	private String testUrl = toShowcaseUrl("separator.jsf");
+
+	@Before
+	public void init() {
+		driver.get(testUrl);
+	}
+
+	@Test
+	public void shouldShowSeparator() {
+		WebElement separator = findElementById("separator");
+		assertTrue(separator.isDisplayed());
+	}
+
+	@Test
+	public void shouldShowCustomSeparator() {
+		WebElement separator = findElementById("customSeparator");
+		assertTrue(separator.isDisplayed());
+		assertTrue(separator.getCssValue("width").equals("498px"));
+		assertTrue(separator.getCssValue("height").equals("18px"));
+	}
+
+}
