1b03e37b8a71fc32810603c7e4ca61f369a7c17e
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-02-24 10:27:45 +0000

    Integration Tests - Keyboard refactor.
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@6992 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/test/java/com/prime/showcase/integration/keyboard/KeyboardCompleteIntegrationTest.java
+++ src/test/java/com/prime/showcase/integration/keyboard/KeyboardCompleteIntegrationTest.java
@@ -46,55 +46,67 @@ public class KeyboardCompleteIntegrationTest extends AbstractIntegrationTest {
     public void shouldUseDefaultKeyboard() throws InterruptedException{
         
         WebElement panel = findElementById("keypad-div");
+        WebElement title = findElementByClass("title");
 
         findElementBySelector(escapeClientId("form:default") + "[type='text']").click();
         Thread.sleep(1000);
         assertTrue("Should display default keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid default keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 74);
 
+        //hide previous keyboard
+        title.click();
+        waitUntilAllAnimationsComplete();
         
+        //test new one
         findElementBySelector(escapeClientId("form:qwerty") + "[type='text']").click();
         Thread.sleep(1000);
         assertTrue("Should display qwerty keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid qwerty keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 30);
 
-        
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:alphabetic") + "[type='text']").click();
         Thread.sleep(1000);
         assertTrue("Should display alphabetic keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid alphabetic keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 30);
 
-        
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:custom1") + "[type='text']").click();
         Thread.sleep(1000);
         assertTrue("Should display custom1 keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid custom1 keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 18);
 
-
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:custom2") + "[type='text']").click();
         Thread.sleep(1000);
         assertTrue("Should display custom2 keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid custom2 keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 30);
 
-
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:keypad") + "[type='text'][readonly='readonly']").click();
         Thread.sleep(1000);
         assertTrue("Should display keypad keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid keypad keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 13);
 
-
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:buttonmode") + "[type='text'][readonly='readonly'] + button").click();
         Thread.sleep(1000);
         assertTrue("Should display buttonmode keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid buttonmode keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 13);
 
-
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:image") + "[type='text'][readonly='readonly'] + img").click();
         Thread.sleep(1000);
         assertTrue("Should display image keyboard panel.", panel.isDisplayed());
         assertTrue("Should render valid image keyboard keys.", findElementsBySelector(panel, ".keypad-row .keypad-key").size() == 13);
 
-
+        title.click();
+        waitUntilAllAnimationsComplete();
         findElementBySelector(escapeClientId("form:password") + "[type='password'][readonly='readonly']").click();
         Thread.sleep(1000);
         assertTrue("Should display password keyboard panel.", panel.isDisplayed());
