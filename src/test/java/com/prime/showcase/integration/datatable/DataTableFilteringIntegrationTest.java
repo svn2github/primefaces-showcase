cc4097d86f49477e2f895041ee32604966b1bb15
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-02-24 15:28:24 +0000

    Integration Tests - DataTableFiltering refactor.
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@7003 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/test/java/com/prime/showcase/integration/datatable/DataTableFilteringIntegrationTest.java
+++ src/test/java/com/prime/showcase/integration/datatable/DataTableFilteringIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.prime.showcase.integration.datatable;
 
+import java.util.NoSuchElementException;
+import org.junit.BeforeClass;
 import java.util.List;
 
 import org.junit.Before;
@@ -10,122 +12,109 @@ import org.openqa.selenium.WebElement;
 import static org.junit.Assert.assertTrue;
 
 public class DataTableFilteringIntegrationTest extends AbstractDataTableIntegrationTest {
-	private static final String DATA_TABLE_DATA = "dataTable_data";
-	@Before
-	public void init() {
-		String testUrl = toShowcaseUrl("datatableFiltering.jsf");
-		driver.get(testUrl);
-	}
-	
-	@Test
-	public void shouldFilterContains() {
-		WebElement filterElement = findElementById("dataTable:modelColumn_filter");
-		WebElement dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rows = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		assertTrue(rows.size() > 0);
-		List<String> models = getModels(rows);
-		String key = String.valueOf(models.get(0).charAt(4));
-		filterElement.sendKeys(key);
-		waitUntilAjaxRequestCompletes();
-		
-		dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rowsAfterFiltering = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		List<String> modelsAfterFiltering = getModels(rowsAfterFiltering);
-		assertTrue(modelsAfterFiltering.size() > 0);
-		for (String eachString : modelsAfterFiltering) {
-			assertTrue(eachString.contains(key));
-		}
-	}
-	
-	@Test
-	public void shouldFilterStarsWith() {
-		WebElement filterElement = findElementById("dataTable:yearColumn_filter");
-		WebElement dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rows = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		assertTrue(rows.size() > 0);
-		List<String> years = getYears(rows);
-		String key = String.valueOf(years.get(0).subSequence(0, 2));
-		filterElement.sendKeys(key);
-		waitUntilAjaxRequestCompletes();
 
-		dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rowsAfterFiltering = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		List<String> yearsAfterFiltering = getYears(rowsAfterFiltering);
-		assertTrue(yearsAfterFiltering.size() > 0);
-		for (String eachString : yearsAfterFiltering) {
-			assertTrue(eachString.startsWith(key));
-		}
-	}
+    public static final String TEST_URL = PRIME_SHOWCASE_UI + "datatableFiltering.jsf";
+    
+    @Before
+    public void init(){
+        driver.get(TEST_URL);
+    }
 	
-	@Test
-	public void shouldFilterExactItems() {
-		WebElement filterSelectElement = findElementById("dataTable:manufacturerColumn_filter");
-		WebElement dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rows = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		assertTrue(rows.size() > 0);
-		List<String> cars = getManufacturers(rows);
-		
-		String selectItemValue = filterSelectElement.getText().split("\n")[3];
-		selectElementByValue(filterSelectElement,selectItemValue);
-		waitUntilAjaxRequestCompletes();
-		
-		dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rowsAfterFiltering = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		int rowSize = rowsAfterFiltering.size();
-		rowSize = setRowSizeToZeroIfTableEmpty(rowsAfterFiltering, rowSize);
-		int exactCarCount = exactCarCount(cars, selectItemValue);
-		assertTrue(exactCarCount == rowSize);
-	}
-
-	private int exactCarCount(List<String> cars, String selectItemValue) {
-		int count = 0;
-		for (String eachCar : cars) {
-			if(eachCar.equalsIgnoreCase(selectItemValue)) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	private int setRowSizeToZeroIfTableEmpty(List<WebElement> rowsAfterFiltering, int rowSize) {
-		if(rowSize == 1) {
-			if(rowsAfterFiltering.get(0).getText().contains("with given criteria")) {
-				rowSize = 0;
-			}
-		}
-		return rowSize;
-	}
-	
-	@Test
-	public void shouldFilterEndsWiths() {
-		WebElement dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rows = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		assertTrue(rows.size() > 0);
-		List<String> colors = getColors(rows);
-		String searchCriteria = String.valueOf(colors.get(0).charAt(colors.get(0).length()-1));
-		findElementById("dataTable:colorColumn_filter").sendKeys(searchCriteria);
-		waitUntilAjaxRequestCompletes();
-		
-		int colorCount = getCarCount(colors, searchCriteria);
-		
-		dataTableElement = findElementById(DATA_TABLE_DATA);
-		List<WebElement> rowsAfterFiltering = dataTableElement.findElements(By.cssSelector(ROW_CLASS));
-		int rowSize = rowsAfterFiltering.size();
-		assertTrue(rowSize == colorCount);
-		List<String> colorsAfterFiltering = getColors(rowsAfterFiltering);
-		for (String car : colorsAfterFiltering) {
-			assertTrue(car.endsWith(searchCriteria));
-		}
-	}
-
-	private int getCarCount(List<String> cars, String searchCriteria) {
-		int count = 0;
-		for (String car : cars) {
-			if(car.endsWith(searchCriteria)) {
-				count++;
-			}
-		}
-		return count;
-	}
-	
+    @Test
+    public void shouldVerifyDOM(){
+        
+        try{
+            
+            findElementById("dataTable");
+            findElementById("dataTable:globalFilter");
+            findElementById("dataTable:modelColumn_filter");
+            findElementById("dataTable:yearColumn_filter");
+            findElementById("dataTable:manufacturerColumn_filter");
+            findElementById("dataTable:colorColumn_filter");
+            
+        }
+        catch(NoSuchElementException e){
+            assertTrue("DataTableFiltering showcase DOM not verified.", false);
+        }
+    }
+    
+    @Test
+    public void shouldContains() {
+        
+        WebElement table = findElementById("dataTable");
+        
+        int validCount = findElementsByXpath(table, "table/tbody//tr/td[1][contains(., 'a')]").size();
+        
+        findElementById("dataTable:modelColumn_filter").sendKeys("a");
+        
+        waitUntilAjaxRequestCompletes();
+        
+        List<WebElement> cols = findElementsByXpath(table, "table/tbody//tr/td[1][contains(., 'a')]");
+        
+        assertTrue("Should find valid amount.", cols.size() == validCount);
+        
+        for (WebElement e : cols) {
+            assertTrue("Should find valid (contains) items.", e.getText().contains("a"));
+        }
+    }
+    
+    @Test
+    public void shouldStartsWith() {
+        
+        WebElement table = findElementById("dataTable");
+        
+        int validCount = findElementsByXpath(table, "table/tbody//tr/td[2][starts-with(., '199')]").size();
+        
+        findElementById("dataTable:yearColumn_filter").sendKeys("199");
+        
+        waitUntilAjaxRequestCompletes();
+        
+        List<WebElement> cols = findElementsByXpath(table, "table/tbody//tr/td[2][starts-with(., '199')]");
+        
+        assertTrue("Should find valid amount.", cols.size() == validCount);
+        
+        for (WebElement e : cols) {
+            assertTrue("Should find valid (starts with) items.", e.getText().startsWith("199"));
+        }
+    }
+    
+    @Test
+    public void shouldExact() {
+        
+        WebElement table = findElementById("dataTable");
+        
+        int validCount = findElementsByXpath(table, "table/tbody//tr/td[3][. = 'Ford']").size();
+        
+        selectElementByValue(findElementById("dataTable:manufacturerColumn_filter"), "Ford");
+        
+        waitUntilAjaxRequestCompletes();
+        
+        List<WebElement> cols = findElementsByXpath(table, "table/tbody//tr/td[3][. = 'Ford']");
+        
+        assertTrue("Should find valid amount.", cols.size() == validCount);
+        
+        for (WebElement e : cols) {
+            assertTrue("Should find valid (exact) items.", e.getText().equals("Ford"));
+        }
+    }
+    
+    @Test
+    public void shouldEndsWith() {
+        
+        WebElement table = findElementById("dataTable");
+        
+        int validCount = findElementsByXpath(table, "table/tbody//tr/td[4][. = 'Yellow']").size();
+        
+        findElementById("dataTable:colorColumn_filter").sendKeys("low");
+        
+        waitUntilAjaxRequestCompletes();
+        
+        List<WebElement> cols = findElementsByXpath(table, "table/tbody//tr/td[4][. = 'Yellow']");
+        
+        assertTrue("Should find valid amount.", cols.size() == validCount);
+        
+        for (WebElement e : cols) {
+            assertTrue("Should find valid (ends with) items.", e.getText().endsWith("low"));
+        }
+    }
 }
