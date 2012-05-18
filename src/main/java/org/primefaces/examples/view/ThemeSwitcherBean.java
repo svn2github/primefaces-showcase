c566cfacc58c7cfaf4d8c92d0867163462d99984
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-05-18 11:48:33 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@7470 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ThemeSwitcherBean.java
+++ src/main/java/org/primefaces/examples/view/ThemeSwitcherBean.java
@@ -54,8 +54,37 @@ public class ThemeSwitcherBean {
         
         advancedThemes = new ArrayList<Theme>();
         advancedThemes.add(new Theme("aristo", "aristo.png"));
+        advancedThemes.add(new Theme("black-tie", "black-tie.png"));
+        advancedThemes.add(new Theme("blitzer", "blitzer.png"));
+        advancedThemes.add(new Theme("bluesky", "bluesky.png"));
+        advancedThemes.add(new Theme("casablanca", "casablanca.png"));
         advancedThemes.add(new Theme("cupertino", "cupertino.png"));
+        advancedThemes.add(new Theme("dark-hive", "dark-hive.png"));
+        advancedThemes.add(new Theme("dot-luv", "dot-luv.png"));
+        advancedThemes.add(new Theme("eggplant", "eggplant.png"));
+        advancedThemes.add(new Theme("excite-bike", "excite-bike.png"));
+        advancedThemes.add(new Theme("flick", "flick.png"));
+        advancedThemes.add(new Theme("glass-x", "glass-x.png"));
+        advancedThemes.add(new Theme("home", "home.png"));
+        advancedThemes.add(new Theme("hot-sneaks", "hot-sneaks.png"));
+        advancedThemes.add(new Theme("humanity", "humanity.png"));
+        advancedThemes.add(new Theme("le-frog", "le-frog.png"));
+        advancedThemes.add(new Theme("midnight", "midnight.png"));
+        advancedThemes.add(new Theme("mint-choc", "mint-choc.png"));
+        advancedThemes.add(new Theme("overcast", "overcast.png"));
+        advancedThemes.add(new Theme("pepper-grinder", "pepper-grinder.png"));
+        advancedThemes.add(new Theme("redmond", "redmond.png"));
+        advancedThemes.add(new Theme("rocket", "rocket.png"));
+        advancedThemes.add(new Theme("sam", "sam.png"));
+        advancedThemes.add(new Theme("smoothness", "smoothness.png"));
+        advancedThemes.add(new Theme("south-street", "south-street.png"));
+        advancedThemes.add(new Theme("start", "start.png"));
+        advancedThemes.add(new Theme("sunny", "sunny.png"));
+        advancedThemes.add(new Theme("swanky-purse", "swanky-purse.png"));
         advancedThemes.add(new Theme("trontastic", "trontastic.png"));
+        advancedThemes.add(new Theme("ui-darkness", "ui-darkness.png"));
+        advancedThemes.add(new Theme("ui-lightness", "ui-lightness.png"));
+        advancedThemes.add(new Theme("vader", "vader.png"));
         
         themes = new TreeMap<String, String>();
         themes.put("Aristo", "aristo");
