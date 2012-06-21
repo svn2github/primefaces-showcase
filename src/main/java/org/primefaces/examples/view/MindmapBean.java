e5f6b55b686e2e02630f8178f9b351da950f0705
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-06-21 09:44:19 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@7650 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/MindmapBean.java
+++ src/main/java/org/primefaces/examples/view/MindmapBean.java
@@ -16,8 +16,6 @@
 package org.primefaces.examples.view;
 
 import java.io.Serializable;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.model.mindmap.DefaultMindmapNode;
 import org.primefaces.model.mindmap.MindmapNode;
@@ -29,25 +27,13 @@ public class MindmapBean implements Serializable {
     public MindmapBean() {
         root = new DefaultMindmapNode("google.com", "CC0000", false);
         
-        MindmapNode ips = new DefaultMindmapNode("IPs", "3399FF", false);
-        MindmapNode ns = new DefaultMindmapNode("NS(s)", "3399FF", false);
-        MindmapNode malware = new DefaultMindmapNode("Malware", "3399FF", false);
+        MindmapNode ips = new DefaultMindmapNode("IPs", "3399FF", true);
+        MindmapNode ns = new DefaultMindmapNode("NS(s)", "3399FF", true);
+        MindmapNode malware = new DefaultMindmapNode("Malware", "3399FF", true);
         
         root.add(ips);
         root.add(ns);
         root.add(malware);
-        
-        //IPs
-        ips.add(new DefaultMindmapNode("1.1.1.1", "00CC00", true));
-        ips.add(new DefaultMindmapNode("1.1.1.2", "00CC00", true));
-        
-        //NSs
-        ns.add(new DefaultMindmapNode("ns1.google.com", "00CC00", true));
-        ns.add(new DefaultMindmapNode("ns2.google.com", "00CC00", true));
-        ns.add(new DefaultMindmapNode("ns3.google.com", "00CC00", true));
-        
-        //Malware
-        malware.add(new DefaultMindmapNode("someMalware", "00CC00", true));
     }
 
     public MindmapNode getRoot() {
@@ -55,8 +41,25 @@ public class MindmapBean implements Serializable {
     }
     
     public void onNodeSelect(SelectEvent event) {
-        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Node selected.", ((MindmapNode) event.getObject()).getData().toString());
+        MindmapNode node = (MindmapNode) event.getObject();
         
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+        //populate if not already loaded
+        if(node.getChildren().isEmpty()) {
+            Object data = node.getData();
+
+            if(data.equals("NS(s)")) {
+                node.add(new DefaultMindmapNode("ns1.google.com", "00CC00", false));
+                node.add(new DefaultMindmapNode("ns2.google.com", "00CC00", false));
+                node.add(new DefaultMindmapNode("ns3.google.com", "00CC00", false));
+            }
+            else if(data.equals("IPs")) {
+                node.add(new DefaultMindmapNode("1.1.1.1", "00CC00", false));
+                node.add(new DefaultMindmapNode("1.1.1.2", "00CC00", false));
+            }
+            else if(data.equals("Malware")) {
+                node.add(new DefaultMindmapNode("someMalware", "00CC00", false));
+            }
+        }
+        
     }
 }
