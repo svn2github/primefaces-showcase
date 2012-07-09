dc0b79be4526d651741218442e24c7c58f59473e
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-07-09 08:16:40 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@7760 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/GrowlBean.java
+++ src/main/java/org/primefaces/examples/view/GrowlBean.java
@@ -3,17 +3,38 @@ package org.primefaces.examples.view;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import org.primefaces.push.PushContext;
+import org.primefaces.push.PushContextFactory;
 
 public class GrowlBean {
 
 	private String text;
-	
+    
+    private String summary;
+    
+    private String detail;
+    
 	public String getText() {
 		return text;
 	}
 	public void setText(String text) {
 		this.text = text;
 	}
+    
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
 
 	public void save(ActionEvent actionEvent) {
 		FacesContext context = FacesContext.getCurrentInstance();
@@ -21,4 +42,10 @@ public class GrowlBean {
 		context.addMessage(null, new FacesMessage("Successful", "Hello " + text));
 		context.addMessage(null, new FacesMessage("Second Message", "Additional Info Here..."));
 	}
+    
+    public void send() {
+        PushContext pushContext = PushContextFactory.getDefault().getPushContext();
+        
+        pushContext.push("/notifications", new FacesMessage(summary, detail));
+    }
 }
