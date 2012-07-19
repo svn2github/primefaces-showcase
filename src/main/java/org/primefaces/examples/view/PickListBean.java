b8cbd5cf5c6888a905c8c14e1235954c5e7ab848
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-07-19 08:55:31 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@7822 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/PickListBean.java
+++ src/main/java/org/primefaces/examples/view/PickListBean.java
@@ -17,6 +17,9 @@ package org.primefaces.examples.view;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.TransferEvent;
 
 import org.primefaces.examples.domain.Player;
 import org.primefaces.model.DualListModel;
@@ -62,9 +65,23 @@ public class PickListBean {
 	}
 	
 	public DualListModel<String> getCities() {
-		return cities;
-	}
+		return cities;	
+}
 	public void setCities(DualListModel<String> cities) {
 		this.cities = cities;
 	}
+    
+    public void onTransfer(TransferEvent event) {
+        StringBuilder builder = new StringBuilder();
+        for(Object item : event.getItems()) {
+            builder.append(((Player) item).getName()).append("<br />");
+        }
+        
+        FacesMessage msg = new FacesMessage();
+        msg.setSeverity(FacesMessage.SEVERITY_INFO);
+        msg.setSummary("Items Transferred");
+        msg.setDetail(builder.toString());
+        
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 }
