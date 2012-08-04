bad88a58cc2026990c0cedee4f20234587d3456c
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-08-04 12:38:16 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@7895 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/TagCloudBean.java
+++ src/main/java/org/primefaces/examples/view/TagCloudBean.java
@@ -15,8 +15,12 @@
  */
 package org.primefaces.examples.view;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.model.tagcloud.DefaultTagCloudItem;
 import org.primefaces.model.tagcloud.DefaultTagCloudModel;
+import org.primefaces.model.tagcloud.TagCloudItem;
 import org.primefaces.model.tagcloud.TagCloudModel;
 
 public class TagCloudBean {
@@ -25,19 +29,25 @@ public class TagCloudBean {
     
     public TagCloudBean() {
         model = new DefaultTagCloudModel();
-        model.addTag(new DefaultTagCloudItem("Transformers", "#", 1));
-        model.addTag(new DefaultTagCloudItem("RIA", "#", 3));
-        model.addTag(new DefaultTagCloudItem("AJAX", "#", 2));
-        model.addTag(new DefaultTagCloudItem("jQuery", "#", 5));
-        model.addTag(new DefaultTagCloudItem("NextGen", "#", 4));
-        model.addTag(new DefaultTagCloudItem("JSF 2.0", "#", 2));
-        model.addTag(new DefaultTagCloudItem("FCB", "#", 5));
-        model.addTag(new DefaultTagCloudItem("Mobile", "#", 3));
-        model.addTag(new DefaultTagCloudItem("Themes", "#", 4));
-        model.addTag(new DefaultTagCloudItem("Rocks", "#", 1));
+        model.addTag(new DefaultTagCloudItem("Transformers", 1));
+        model.addTag(new DefaultTagCloudItem("RIA", "/ui/tagCloud.jsf", 3));
+        model.addTag(new DefaultTagCloudItem("AJAX", 2));
+        model.addTag(new DefaultTagCloudItem("jQuery", "/ui/tagCloud.jsf", 5));
+        model.addTag(new DefaultTagCloudItem("NextGen", 4));
+        model.addTag(new DefaultTagCloudItem("JSF 2.0", "/ui/tagCloud.jsf", 2));
+        model.addTag(new DefaultTagCloudItem("FCB", 5));
+        model.addTag(new DefaultTagCloudItem("Mobile",  3));
+        model.addTag(new DefaultTagCloudItem("Themes", "/ui/tagCloud.jsf", 4));
+        model.addTag(new DefaultTagCloudItem("Rocks", "/ui/tagCloud.jsf", 1));
     }
 
     public TagCloudModel getModel() {
         return model;
+    }
+    
+    public void onSelect(SelectEvent event) {
+        TagCloudItem item = (TagCloudItem) event.getObject();
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item Selected", item.getLabel());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 }
