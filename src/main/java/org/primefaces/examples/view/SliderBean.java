36fb2ed194ea5dfd98357132a79d746f5590b511
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-12-24 19:42:12 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@8522 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/SliderBean.java
+++ src/main/java/org/primefaces/examples/view/SliderBean.java
@@ -30,11 +30,14 @@ public class SliderBean {
 	private int number4;
 
     private int number5;
+    
+    private int number6 = 30;
+    
+    private int number7 = 80;
 
 	public int getNumber1() {
 		return number1;
 	}
-
 	public void setNumber1(int number1) {
 		this.number1 = number1;
 	}
@@ -42,7 +45,6 @@ public class SliderBean {
 	public int getNumber2() {
 		return number2;
 	}
-
 	public void setNumber2(int number2) {
 		this.number2 = number2;
 	}
@@ -50,7 +52,6 @@ public class SliderBean {
 	public int getNumber3() {
 		return number3;
 	}
-
 	public void setNumber3(int number3) {
 		this.number3 = number3;
 	}
@@ -58,7 +59,6 @@ public class SliderBean {
 	public int getNumber4() {
 		return number4;
 	}
-
 	public void setNumber4(int number4) {
 		this.number4 = number4;
 	}
@@ -66,11 +66,26 @@ public class SliderBean {
     public int getNumber5() {
         return number5;
     }
-
     public void setNumber5(int number5) {
         this.number5 = number5;
     }
-    
+
+    public int getNumber6() {
+        return number6;
+    }
+
+    public void setNumber6(int number6) {
+        this.number6 = number6;
+    }
+
+    public int getNumber7() {
+        return number7;
+    }
+
+    public void setNumber7(int number7) {
+        this.number7 = number7;
+    }
+
     public void onSlideEnd(SlideEndEvent event) {
         FacesMessage msg = new FacesMessage("Slide Ended", "Value: " + event.getValue());
         FacesContext.getCurrentInstance().addMessage(null, msg);
