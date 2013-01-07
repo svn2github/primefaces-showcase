5998e7f955f3443e9f1b7bd7c8fc82492c29d3ab
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-01-07 14:51:26 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@8598 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/FormBean.java
+++ src/main/java/org/primefaces/examples/view/FormBean.java
@@ -16,11 +16,14 @@
 package org.primefaces.examples.view;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 
 public class FormBean implements Serializable {
 
@@ -31,6 +34,10 @@ public class FormBean implements Serializable {
 	private List<String> selectedMovies;
 
 	private List<String> selectedOptions;
+    
+    private String car;
+    
+    private List<SelectItem> cars;
 
 	private Map<String, String> movies;
 
@@ -44,6 +51,16 @@ public class FormBean implements Serializable {
 		movies.put("Goodfellas", "Goodfellas");
 		movies.put("Godfather", "Godfather");
 		movies.put("Carlito's Way", "Carlito's Way");
+        
+        SelectItemGroup g1 = new SelectItemGroup("German Cars");
+        g1.setSelectItems(new SelectItem[] {new SelectItem("BMW", "BMW"), new SelectItem("Mercedes", "Mercedes"), new SelectItem("Volkswagen", "Volkswagen")});
+        
+        SelectItemGroup g2 = new SelectItemGroup("American Cars");
+        g2.setSelectItems(new SelectItem[] {new SelectItem("Chrysler", "Chrysler"), new SelectItem("GM", "GM"), new SelectItem("Ford", "Ford")});
+        
+        cars = new ArrayList<SelectItem>();
+        cars.add(g1);
+        cars.add(g2);
 	}
 
 	public boolean isValue1() {
@@ -97,6 +114,18 @@ public class FormBean implements Serializable {
 	public void setNumber(Integer number) {
 		this.number = number;
 	}
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public List<SelectItem> getCars() {
+        return cars;
+    }
 
 	public void addMessage() {
 		String summary = value2 ? "Checked" : "Unchecked";
