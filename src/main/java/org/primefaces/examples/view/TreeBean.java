3c4fac47336e9643eba3be5b30b1a8ff406651ef
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-06-05 17:47:33 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@9337 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/TreeBean.java
+++ src/main/java/org/primefaces/examples/view/TreeBean.java
@@ -19,6 +19,10 @@ import java.io.Serializable;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import org.primefaces.event.NodeCollapseEvent;
+import org.primefaces.event.NodeExpandEvent;
+import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.event.NodeUnselectEvent;
 
 import org.primefaces.event.TreeDragDropEvent;
 import org.primefaces.model.DefaultTreeNode;
@@ -92,6 +96,30 @@ public class TreeBean implements Serializable {
 
 	public void setSelectedNode(TreeNode selectedNode) {
 		this.selectedNode = selectedNode;
+	}
+    
+    	public void onNodeExpand(NodeExpandEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Expanded", event.getTreeNode().toString());
+
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void onNodeCollapse(NodeCollapseEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Collapsed", event.getTreeNode().toString());
+
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void onNodeSelect(NodeSelectEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", event.getTreeNode().toString());
+
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+    public void onNodeUnselect(NodeUnselectEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Unselected", event.getTreeNode().toString());
+
+		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 
     public void onDragDrop(TreeDragDropEvent event) {
