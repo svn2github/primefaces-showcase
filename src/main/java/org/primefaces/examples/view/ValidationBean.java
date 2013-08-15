ffe4f71114768fc38fe873cbab6a04d180d42bab
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-08-15 21:36:10 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@9836 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ValidationBean.java
+++ src/main/java/org/primefaces/examples/view/ValidationBean.java
@@ -1,13 +1,22 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2009-2012 Prime Teknoloji.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.primefaces.examples.view;
 
-/**
- *
- * @author jagatai
- */
+import java.util.Date;
+
 public class ValidationBean {
     
     private String text;
@@ -19,6 +28,8 @@ public class ValidationBean {
     private Float floatNumber;
     
     private String regexText;
+        
+    private Date date;
 
     public String getText() {
         return text;
@@ -59,4 +70,12 @@ public class ValidationBean {
     public void setRegexText(String regexText) {
         this.regexText = regexText;
     }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }    
 }
