07100a192fc03e4aaeecd69030ede08a277a287b
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-01-22 16:56:08 +0000

    New twitter search demo for Mobile
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@10590 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ src/main/java/org/primefaces/examples/mobile/TwitterView.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2009-2014 PrimeTek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.examples.mobile;
+
+import java.util.List;
+import twitter4j.Query;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class TwitterView {
+    	
+	private String keyword;
+    
+    private List<Status> tweets;
+
+    public String getKeyword() {
+        return keyword;
+    }
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public List<Status> getTweets() {
+        return tweets;
+    }
+    public void setTweets(List<Status> tweets) {
+        this.tweets = tweets;
+    }
+
+    public void search() {
+        try {
+            Twitter twitter = TwitterFactory.getSingleton();
+            Query query = new Query(keyword);
+            tweets = twitter.search(query).getTweets();
+        } catch(TwitterException exception) {
+            //TODO: add facesmessage
+        }
+    }
+}
