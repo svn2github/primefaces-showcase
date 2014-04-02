58bae6f2205559e9957b0d48dfc15f66b9a5916a
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-04-02 19:43:26 +0000

    Demo update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@10984 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ChartBean.java
+++ src/main/java/org/primefaces/examples/view/ChartBean.java
@@ -53,7 +53,8 @@ public class ChartBean implements Serializable {
     private HorizontalBarChartModel horizontalBarModel;
     private PieChartModel pieModel;
     private PieChartModel pieModel2;
-    private DonutChartModel donutModel;
+    private DonutChartModel donutModel1;
+    private DonutChartModel donutModel2;
     private MeterGaugeChartModel meterGaugeModel;
     private BubbleChartModel bubbleModel;
     private OhlcChartModel ohlcModel;
@@ -69,7 +70,7 @@ public class ChartBean implements Serializable {
         createPieModel();
         createPieModel2();
         createLivePieModel();
-        createDonutModel();
+        createDonutModels();
         createBubbleModel();
         createOhlcModel();
         createOhlcModel2();
@@ -114,8 +115,12 @@ public class ChartBean implements Serializable {
         return meterGaugeModel;
     }
     
-    public DonutChartModel getDonutModel() {
-        return donutModel;
+    public DonutChartModel getDonutModel1() {
+        return donutModel1;
+    }
+    
+    public DonutChartModel getDonutModel2() {
+        return donutModel2;
     }
     
     public CartesianChartModel getFillToZero() {
@@ -440,29 +445,45 @@ public class ChartBean implements Serializable {
         pieModel2.setDiameter(150);
     }
     
-    private void createDonutModel() {
-        donutModel = new DonutChartModel();
+    private void createDonutModels() {
+        donutModel1 = initDonutModel();
+        donutModel1.setTitle("Donut Chart");
+        donutModel1.setLegendPosition("w");
+        
+        donutModel2 = initDonutModel();
+        donutModel2.setTitle("Custom Options");
+        donutModel2.setLegendPosition("e");
+        donutModel2.setSliceMargin(5);
+        donutModel2.setShowDataLabels(true);
+        donutModel2.setDataFormat("value");
+        donutModel2.setShadow(false);
+    }
+    
+    private DonutChartModel initDonutModel() {
+        DonutChartModel model = new DonutChartModel();
         
         Map<String, Number> circle1 = new LinkedHashMap<String, Number>();
         circle1.put("Brand 1", 150);
         circle1.put("Brand 2", 400);
         circle1.put("Brand 3", 200);
         circle1.put("Brand 4", 10);
-        donutModel.addCircle(circle1);
+        model.addCircle(circle1);
         
         Map<String, Number> circle2 = new LinkedHashMap<String, Number>();
         circle2.put("Brand 1", 540);
         circle2.put("Brand 2", 125);
         circle2.put("Brand 3", 702);
         circle2.put("Brand 4", 421);
-        donutModel.addCircle(circle2);
+        model.addCircle(circle2);
         
         Map<String, Number> circle3 = new LinkedHashMap<String, Number>();
         circle3.put("Brand 1", 40);
         circle3.put("Brand 2", 325);
         circle3.put("Brand 3", 402);
         circle3.put("Brand 4", 421);
-        donutModel.addCircle(circle3);
+        model.addCircle(circle3);
+        
+        return model;
     }
 
     private void createLivePieModel() {
