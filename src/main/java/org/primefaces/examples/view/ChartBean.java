750f635325ef392d1168facf5a69657f8563bc34
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-04-02 18:13:24 +0000

    Demo update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@10981 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ChartBean.java
+++ src/main/java/org/primefaces/examples/view/ChartBean.java
@@ -350,12 +350,23 @@ public class ChartBean implements Serializable {
         ohlcModel.add(new OhlcChartSeries(2011, 136.01, 139.5, 134.53, 139.48));
         ohlcModel.add(new OhlcChartSeries(2012, 124.76, 135.9, 124.55, 135.81));
         ohlcModel.add(new OhlcChartSeries(2012, 123.73, 129.31, 121.57, 122.5));
+        
+        ohlcModel.setTitle("OHLC Chart");
+        ohlcModel.getAxis(AxisType.X).setLabel("Year");
+        ohlcModel.getAxis(AxisType.Y).setLabel("Price Change $K/Unit");
     }
     
     private void createOhlcModel2(){
         ohlcModel2 = new OhlcChartModel();
-        for( int i=1 ; i < 41 ; i++)
-            ohlcModel2.addRecord(new OhlcChartSeries(i, Math.random() * 80 + 80, Math.random() * 50 + 110, Math.random() * 20 + 80, Math.random() * 80 + 80));
+        
+        for( int i=1 ; i < 41 ; i++) {
+            ohlcModel2.add(new OhlcChartSeries(i, Math.random() * 80 + 80, Math.random() * 50 + 110, Math.random() * 20 + 80, Math.random() * 80 + 80));
+        }
+        
+        ohlcModel2.setTitle("Candlestick");
+        ohlcModel2.setCandleStick(true);
+        ohlcModel2.getAxis(AxisType.X).setLabel("Sector");
+        ohlcModel2.getAxis(AxisType.Y).setLabel("Index Value");
     }
     
     private void createBubbleModel(){
