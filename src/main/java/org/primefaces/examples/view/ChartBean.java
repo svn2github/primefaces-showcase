997035143ce9933fe1adfb95742f39477ded3df0
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-04-03 15:23:04 +0000

    Demo update
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@11001 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/view/ChartBean.java
+++ src/main/java/org/primefaces/examples/view/ChartBean.java
@@ -33,6 +33,7 @@ import org.primefaces.model.chart.BubbleChartSeries;
 import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.DateAxis;
 import org.primefaces.model.chart.DonutChartModel;
 import org.primefaces.model.chart.HorizontalBarChartModel;
 import org.primefaces.model.chart.LineChartSeries;
@@ -65,6 +66,7 @@ public class ChartBean implements Serializable {
     private CartesianChartModel animatedModel1;
     private BarChartModel animatedModel2;
     private CartesianChartModel multiAxisModel;
+    private CartesianChartModel dateModel;
 
 	public ChartBean() {
         createLineModels();
@@ -79,6 +81,7 @@ public class ChartBean implements Serializable {
         createAnimatedModels();
         createCombinedModel();
         createMultiAxisModel();
+        createDateModel();
 	}
 
 	public void itemSelect(ItemSelectEvent event) {
@@ -172,6 +175,10 @@ public class ChartBean implements Serializable {
         return multiAxisModel;
     }
 
+    public CartesianChartModel getDateModel() {
+        return dateModel;
+    }
+    
     public PieChartModel getLivePieModel() {
         int random1 = (int)(Math.random() * 1000);
 		int random2 = (int)(Math.random() * 1000);
@@ -628,5 +635,41 @@ public class ChartBean implements Serializable {
         yAxis = animatedModel2.getAxis(AxisType.Y);
         yAxis.setMin(0);
         yAxis.setMax(200);
+    }
+    
+    private void createDateModel() {
+        dateModel = new CartesianChartModel();
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+
+        series1.set("2014-01-01", 51);
+        series1.set("2014-01-06", 22);
+        series1.set("2014-01-12", 65);
+        series1.set("2014-01-18", 74);
+        series1.set("2014-01-24", 24);
+        series1.set("2014-01-30", 51);
+
+        LineChartSeries series2 = new LineChartSeries();
+        series2.setLabel("Series 2");
+
+        series2.set("2014-01-01", 32);
+        series2.set("2014-01-06", 73);
+        series2.set("2014-01-12", 24);
+        series2.set("2014-01-18", 12);
+        series2.set("2014-01-24", 74);
+        series2.set("2014-01-30", 62);
+
+        dateModel.addSeries(series1);
+        dateModel.addSeries(series2);
+        
+        dateModel.setTitle("Zoom for Details");
+        dateModel.setZoom(true);
+        dateModel.getAxis(AxisType.Y).setLabel("Values");
+        DateAxis axis = new DateAxis("Dates");
+        axis.setTickAngle(-50);
+        axis.setMax("2014-02-01");
+        axis.setTickFormat("%b %#d, %y");
+        
+        dateModel.getAxes().put(AxisType.X, axis);
     }
 }
