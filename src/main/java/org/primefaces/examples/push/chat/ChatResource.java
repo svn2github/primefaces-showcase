cfe65dcb3ccd7a68e9b91f07512430738b8bf87f
jfarcand.oss <jfarcand.oss@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-04-11 18:44:31 +0000

    Use the new @PathParam annotation
    git-svn-id: http://primefaces.googlecode.com/svn/examples/trunk/showcase@11046 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/examples/push/chat/ChatResource.java
+++ src/main/java/org/primefaces/examples/push/chat/ChatResource.java
@@ -20,6 +20,7 @@ import org.primefaces.push.RemoteEndpoint;
 import org.primefaces.push.annotation.OnClose;
 import org.primefaces.push.annotation.OnMessage;
 import org.primefaces.push.annotation.OnOpen;
+import org.primefaces.push.annotation.PathParam;
 import org.primefaces.push.annotation.PushEndpoint;
 import org.primefaces.push.annotation.Singleton;
 import org.slf4j.Logger;
@@ -34,9 +35,11 @@ public class ChatResource {
 
     private final Logger logger = LoggerFactory.getLogger(ChatResource.class);
 
-    // The Path takes the form of /{room}/{user}
-    public final static int USER_PATH_SEGMENT = 2;
-    public final static int ROOM_PATH_SEGMENT = 1;
+    @PathParam("room")
+    private String room;
+
+    @PathParam("user")
+    private String username;
 
     @Inject
     private ServletContext ctx;
@@ -44,18 +47,12 @@ public class ChatResource {
     @OnOpen
     public void onOpen(RemoteEndpoint r, EventBus eventBus) {
         logger.info("OnOpen {}", r);
-        
-        String username = r.pathSegments(USER_PATH_SEGMENT);
-        String room = r.pathSegments(ROOM_PATH_SEGMENT);
 
         eventBus.publish(room + "/*", new Message(String.format("%s has entered the room '%s'",  username, room), true));
     }
 
     @OnClose
     public void onClose(RemoteEndpoint r, EventBus eventBus) {
-        String username = r.pathSegments(USER_PATH_SEGMENT);
-        String room = r.pathSegments(ROOM_PATH_SEGMENT);
-        
         ChatUsers users= (ChatUsers) ctx.getAttribute("chatUsers");
         users.remove(username);
         
